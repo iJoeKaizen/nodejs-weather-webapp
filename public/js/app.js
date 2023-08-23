e7b78d97774d3487fffff615d3debc426bfdfad9
iJoeKaizen <mrgmens@gmail.com>
2023-08-23 10:41:09 +0100

    Removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript");
-
 const mainContent = document.querySelector(".main-content");
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
