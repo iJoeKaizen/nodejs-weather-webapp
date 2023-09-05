92c9c600b60f16d6c75311c08f980f99ccf15750
iJoeKaizen <mrgmens@gmail.com>
2023-09-05 15:31:57 +0100

    Setup for Cyclic deployement

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener("submit", (e) => {
 
   displayInfo.innerHTML = `<p>Loading...</p>`;
 
-  fetch(`http://localhost:3000/weather?search=${location}`)
+  fetch(`/weather?search=${location}`)
     .then((response) => {
       return response.json();
     })
